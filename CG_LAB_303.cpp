68c203a19e24a7b464b1d5193fc5c372980b9cc6
andrey-avdeev-gh <56041237+andrey-avdeev-gh@users.noreply.github.com>
2020-12-17 17:13:58 +0300

    Add files via upload

--- CG_LAB_303.cpp
+++ CG_LAB_303.cpp
@@ -14,215 +14,275 @@ int main();
 
 Mat chooseimg()
 {
-    cout << endl << "Enter full name of the file: " << endl;
-    string imagename = "";
-    cin >> imagename;
+	cout << endl << "Enter full name of the file: " << endl;
+	string imagename = "";
+	cin >> imagename;
 
-    try
-    {
-        Mat img = imread(samples::findFile(imagename), IMREAD_COLOR);
-        if (img.empty())
-        {
-            cout << "Image cannot be loaded..!!" << endl;
-            exit(0);
-        }
+	try
+	{
+		Mat img = imread(samples::findFile(imagename), IMREAD_COLOR);
+		if (img.empty())
+		{
+			cout << "Image cannot be loaded..!!" << endl;
+			exit(0);
+		}
 
-        return img;
-    }
-    catch (cv::Exception)
-    {
-        cout << "Image cannot be loaded..!!" << endl;
-        exit(0);
-    }
+		return img;
+	}
+	catch (cv::Exception)
+	{
+		cout << "Image cannot be loaded..!!" << endl;
+		exit(0);
+	}
 
 
 }
 
 
-int task_1()
+void task_1()
 {
-    Mat src = chooseimg();
-    resize(src, src, Size(512, 512));
-    src.convertTo(src, CV_32F, 1.0 / 255);
-    Mat srcDCT;
-    dct(src, srcDCT);
+	IplImage* image = 0;
+	IplImage* gray = 0;
+	IplImage* dst = 0;
 
-    imshow("src", src);
-    imshow("dct", srcDCT);
-    waitKey();
-    return 0;
-    main();
+		// имя картинки задаётся первым параметром
+		char* filename = argc == 2 ? argv[1] : "Image0.jpg";
+		// получаем картинку
+		image = cvLoadImage(filename, 1);
+
+		printf("[i] image: %s\n", filename);
+		assert(image != 0);
+
+		// создаём одноканальные картинки
+		gray = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+		dst = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+
+		// окно для отображения картинки
+		cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
+		cvNamedWindow("gray", CV_WINDOW_AUTOSIZE);
+		cvNamedWindow("cvCanny", CV_WINDOW_AUTOSIZE);
+
+		// преобразуем в градации серого
+		cvCvtColor(image, gray, CV_RGB2GRAY);
+
+		// получаем границы
+		cvCanny(gray, dst, 10, 100, 3);
+
+		// показываем картинки
+		cvShowImage("original", image);
+		cvShowImage("gray", gray);
+		cvShowImage("cvCanny", dst);
+
+		// ждём нажатия клавиши
+		cvWaitKey(0);
+
+		// освобождаем ресурсы
+		cvReleaseImage(&image);
+		cvReleaseImage(&gray);
+		cvReleaseImage(&dst);
+		// удаляем окна
+		cvDestroyAllWindows();
 }
 
 
-int task_2(IplImage* original, float accuracy = 0.1)
+void task_2()
 {
-	assert(original != 0);
+	IplImage* image = 0;
+	// имя картинки задаётся первым параметром
+	char* filename = argc >= 2 ? argv[1] : "Image0.jpg";
+	// получаем картинку (в градациях серого)
+	image = cvLoadImage(filename, CV_LOAD_IMAGE_GRAYSCALE);
 
-	IplImage* src = 0, * rgb = 0;
-	IplImage* bin = 0;
-	IplImage* phase = 0;
+	printf("[i] image: %s\n", filename);
+	assert(image != 0);
 
-	src = cvCloneImage(original);
+	// загрузим оригинальное изображении
+	IplImage* src = cvLoadImage(filename);
 
-	// заведём цветную картинку
-	rgb = CreateImage(GetSize(src), 8, 3);
-	cvConvertImage(src, rgb, GRAY2BGR);
-
-	// бинарная картинка - для контуров
-	bin = CreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
-	cvCanny(src, bin, 50, 200);
-
-	cvNamedWindow("bin", 1);
-	cvShowImage("bin", bin);
-
-	// максимальное расстояние от начала координат - это длина диагонали
-	int RMax = cvRound(sqrt((double)(src->width * src->width + src->height * src->height)));
-
-	// картинка для хранения фазового пространства Хафа (r, f)
-	// 0 < r < RMax
-	// 0 < f < 2*PI
-	phase = cvCreateImage(cvSize(RMax, 180), IPL_DEPTH_16U, 1);
-	cvZero(phase);
-
-	int x = 0, y = 0, r = 0, f = 0;
-	float theta = 0;
-
-	// пробегаемся по пикселям изображения контуров
-	for (y = 0; y < bin->height; y++) {
-		uchar* ptr = (uchar*)(bin->imageData + y * bin->widthStep);
-		for (x = 0; x < bin->width; x++) {
-			if (ptr[x] > 0) { // это пиксель контура?
-				// рассмотрим все возможные прямые, которые могут 
-				// проходить через эту точку
-				for (f = 0; f < 180; f++) { //перебираем все возможные углы наклона
-					short* ptrP = (short*)(phase->imageData + f * phase->widthStep);
-					for (r = 0; r < RMax; r++) { // перебираем все возможные расстояния от начала координат
-						theta = f * CV_PI / 180.0; // переводим градусы в радианы
-
-						// Если решение уравнения достаточно хорошее (точность больше заданой)
-						if (abs(((y)*sin(theta) + (x)*cos(theta)) - r) < accuracy) {
-							ptrP[r]++; // увеличиваем счетчик для этой точки фазового пространства.
-						}
-					}
-				}
-			}
-		}
+	// хранилище памяти для кругов
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	// сглаживаем изображение
+	cvSmooth(image, image, CV_GAUSSIAN, 5, 5);
+	// поиск кругов
+	CvSeq* results = cvHoughCircles(
+		image,
+		storage,
+		CV_HOUGH_GRADIENT,
+		10,
+		image->width / 5
+	);
+	// пробегаемся по кругам и рисуем их на оригинальном изображении
+	for (int i = 0; i < results->total; i++) {
+		float* p = (float*)cvGetSeqElem(results, i);
+		CvPoint pt = cvPoint(cvRound(p[0]), cvRound(p[1]));
+		cvCircle(src, pt, cvRound(p[2]), CV_RGB(0xff, 0, 0));
 	}
+	// показываем
+	cvNamedWindow("cvHoughCircles", 1);
+	cvShowImage("cvHoughCircles", src);
 
-	NamedWindow("phase", 1);
-	ShowImage("phase", phase);
-
-	// увеличим фазовую картинку
-	IplImage* phaseImage = CreateImage(cvSize(phase->width * 3, phase->height * 3), IPL_DEPTH_16U, 1);
-	cvResize(phase, phaseImage);
-
-	cvNamedWindow("phaseImage", 1);
-	cvShowImage("phaseImage", phaseImage);
-
-	// Выбираем точку фазового пространства которая набрала наибольшее число попаданий
-	unsigned int MaxPhaseValue = 0;
-	float Theta = 0;
-	int R = 0;
-	for (f = 0; f < 180; f++) { //перебираем все возможные углы наклона
-		short* ptrP = (short*)(phase->imageData + f * phase->widthStep);
-		for (r = 0; r < RMax; r++) { // перебираем все возможные расстояния от начала координат
-			if (ptrP[r] > MaxPhaseValue) {
-				MaxPhaseValue = ptrP[r];
-				Theta = f;
-				R = r;
-			}
-		}
-	}
-	printf("[M] %d\n", MaxPhaseValue);
-
-	// нормировка
-	float scaler = 0xFFFFFFFF / (float)MaxPhaseValue;
-	for (y = 0; y < phaseImage->height; y++) {
-		short* ptr = (short*)(phaseImage->imageData + y * phaseImage->widthStep);
-		for (x = 0; x < phaseImage->width; x++) {
-			ptr[x] *= scaler;
-		}
-	}
-	cvNamedWindow("phaseImage2", 1);
-	cvShowImage("phaseImage2", phaseImage);
-
-
-	// Рисуем линию по точкам для  R, Teta которые получили в результате преобразования
-	Theta = Theta * CV_PI / 180.0;
-	for (y = 0; y < rgb->height; y++) {
-		uchar* ptr = (uchar*)(rgb->imageData + y * rgb->widthStep);
-		for (x = 0; x < rgb->width; x++) {
-			if (cvRound(((y)*sin(Theta) + (x)*cos(Theta))) == R) {
-				ptr[3 * x] = 0;
-				ptr[3 * x + 1] = 0;
-				ptr[3 * x + 2] = 255;
-			}
-		}
-	}
-
-	NamedWindow("line", 1);
-	ShowImage("line", rgb);
+	// ждём нажатия клавиши
+	cvWaitKey(0);
 
 	// освобождаем ресурсы
-	ReleaseImage(&src);
-	ReleaseImage(&rgb);
-
-	ReleaseImage(&bin);
-	ReleaseImage(&phase);
-	ReleaseImage(&phaseImage);
+	cvReleaseMemStorage(&storage);
+	cvReleaseImage(&image);
+	cvReleaseImage(&src);
+	cvDestroyAllWindows();
 }
 
 
-int task_3() 
+void task_2_var2()
 {
-    return 0;
-    main();
+	IplImage* src = 0;
+	IplImage* dst = 0;
+	IplImage* color_dst = 0;
+
+	// имя картинки задаётся первым параметром
+	char* filename = argc >= 2 ? argv[1] : "Image0.jpg";
+	// получаем картинку (в градациях серого)
+	src = cvLoadImage(filename, CV_LOAD_IMAGE_GRAYSCALE);
+
+	if (!src) {
+		printf("[!] Error: cant load image: %s \n", filename);
+		return -1;
+	}
+
+	printf("[i] image: %s\n", filename);
+
+
+	// хранилище памяти для хранения найденных линий
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	CvSeq* lines = 0;
+	int i = 0;
+
+
+	dst = cvCreateImage(cvGetSize(src), 8, 1);
+	color_dst = cvCreateImage(cvGetSize(src), 8, 3);
+
+	// детектирование границ
+	cvCanny(src, dst, 50, 200, 3);
+
+	// конвертируем в цветное изображение
+	cvCvtColor(dst, color_dst, CV_GRAY2BGR);
+
+	// нахождение линий
+	lines = cvHoughLines2(dst, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI / 180, 50, 50, 10);
+
+	// нарисуем найденные линии
+	for (i = 0; i < lines->total; i++) {
+		CvPoint* line = (CvPoint*)cvGetSeqElem(lines, i);
+		cvLine(color_dst, line[0], line[1], CV_RGB(255, 0, 0), 3, CV_AA, 0);
+	}
+
+	// показываем
+	cvNamedWindow("Source", 1);
+	cvShowImage("Source", src);
+
+	cvNamedWindow("Hough", 1);
+	cvShowImage("Hough", color_dst);
+
+	// ждём нажатия клавиши
+	cvWaitKey(0);
+
+	// освобождаем ресурсы
+	cvReleaseMemStorage(&storage);
+	cvReleaseImage(&src);
+	cvReleaseImage(&dst);
+	cvReleaseImage(&color_dst);
+	cvDestroyAllWindows();
+	main();
+}
+
+void task_3() 
+{
+	// имя картинки задаётся первым параметром
+	char* filename = argc >= 2 ? argv[1] : "Image0.jpg";
+	// получаем картинку
+	image = cvLoadImage(filename, 1);
+
+	printf("[i] image: %s\n", filename);
+	assert(image != 0);
+
+	// создаём одноканальные картинки
+	gray = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+	bin = cvCreateImage(cvGetSize(image), IPL_DEPTH_8U, 1);
+	// клонируем
+	dst = cvCloneImage(image);
+	// окно для отображения картинки
+	cvNamedWindow("original", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("binary", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("contours", CV_WINDOW_AUTOSIZE);
+
+	// преобразуем в градации серого
+	cvCvtColor(image, gray, CV_RGB2GRAY);
+
+	// преобразуем в двоичное
+	cvInRangeS(gray, cvScalar(40), cvScalar(150), bin); // atoi(argv[2])
+
+	CvMemStorage* storage = cvCreateMemStorage(0);
+	CvSeq* contours = 0;
+
+	// находим контуры
+	int contoursCont = cvFindContours(bin, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+
+	// нарисуем контуры
+	for (CvSeq* seq0 = contours; seq0 != 0; seq0 = seq0->h_next) {
+		cvDrawContours(dst, seq0, CV_RGB(255, 216, 0), CV_RGB(0, 0, 250), 0, 1, 8); // рисуем контур
+	}
+
+	// показываем картинки
+	cvShowImage("original", image);
+	cvShowImage("binary", bin);
+	cvShowImage("contours", dst);
+
+	// ждём нажатия клавиши
+	cvWaitKey(0);
+
+	// освобождаем ресурсы
+	cvReleaseImage(&image);
+	cvReleaseImage(&gray);
+	cvReleaseImage(&bin);
+	cvReleaseImage(&dst);
+	// удаляем окна
+	cvDestroyAllWindows();
 }
 
 int main()
-{ 
-    int opt;
+{
+	int opt;
 
-    cout
-        << endl
-        << "   Main Menu    " << endl
-        << endl << "Write 1 for task_1" << endl
-        << endl << "Write 2 for task_2" << endl
-        << endl << "Write 6 for task_3" << endl
-        << endl
-        << endl << "Write 0 to exit" << endl
-        << endl
-        ;
+	cout
+		<< endl
+		<< "   Main Menu    " << endl
+		<< endl << "Write 1 for task_1" << endl
+		<< endl << "Write 2 for task_2" << endl
+		<< endl << "Write 6 for task_3" << endl
+		<< endl
+		<< endl << "Write 0 to exit" << endl
+		<< endl
+		;
 
-    cin >> opt;
-    cout << "\n" << endl;
+	cin >> opt;
+	cout << "\n" << endl;
 
-    switch (opt)
-    {
-    case 1:
-    task_1();
-    break;
-    case 2:
-	IplImage * original = 0;
-		char* filename = argc >= 2 ? argv[1] : "Image0.jpg";
-		original = cvLoadImage(filename, 0);
-    task_2(IplImage * original = 0, float accuracy);
-    break;
-    case 3:
-    task_3();
-    break;
-    case 0:
-    exit(1);
-    default:
-    break;
-    }
-
-
-    return 0;
+	switch (opt)
+	{
+	case 1:
+		task_1();
+		break;
+	case 2:
+		task_2();
+		break;
+	case 3:
+		task_2_var2();
+		break;
+	case 4:
+		task_3();
+		break;
+	case 0:
+		exit(1);
+	default:
+		break;
+	}
 }
-
-
-
-
-
