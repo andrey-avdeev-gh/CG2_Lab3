132f50156b001583fac550888b412a35e8968b75
andrey-avdeev-gh <56041237+andrey-avdeev-gh@users.noreply.github.com>
2020-12-17 19:54:46 +0300

    Add files via upload

--- CG_LAB_303.cpp
+++ CG_LAB_303.cpp
@@ -60,67 +60,34 @@ void task_1()
 		imshow("cvCanny", dst);
 
 		waitKey(0);
-
 		destroyAllWindows();
+		main();
 }
 
 
 void task_2()
 {
 	Mat src = chooseimg();
-	Mat image = src;
-
-	GaussianBlur(image, image, 5.0f, 5);
-
-	CvSeq* results = cvHoughCircles(image,storage,CV_HOUGH_GRADIENT,10,	image->width / 5);
-	for (int i = 0; i < results->total; i++) {
-		float* p = (float*)GetSeqElem(results, i);
-		Point pt = Point(cvRound(p[0]), cvRound(p[1]));
-		circle(src, pt, cvRound(p[2]), CV_RGB(0xff, 0, 0));
+	Mat gray;
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+	medianBlur(gray, gray, 5);
+	vector<Vec3f> circles;
+	HoughCircles(gray, circles, HOUGH_GRADIENT, 1,
+		gray.rows / 16,
+		100, 30, 1, 30
+	);
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		Vec3i c = circles[i];
+		Point center = Point(c[0], c[1]);
+		// circle center
+		circle(src, center, 1, Scalar(0, 100, 100), 3, LINE_AA);
+		// circle outline
+		int radius = c[2];
+		circle(src, center, radius, Scalar(255, 0, 255), 3, LINE_AA);
 	}
-
-	namedWindow("cvHoughCircles", 1);
-	imshow("cvHoughCircles", src);
-
-	waitKey(0);
-	destroyAllWindows();
-	main();
-}
-
-
-void task_2_var2()
-{
-	Mat image = chooseimg(), src, dst, color_dst;
-
-	src = Mat::zeros(image.rows, image.cols, CV_8UC1);
-	dst = Mat::zeros(image.rows, image.cols, CV_8UC1);
-
-	CvMemStorage* storage = cvCreateMemStorage(0);
-	CvSeq* lines = 0;
-	int i = 0;
-
-
-	dst = cvCreateImage(cvGetSize(src), 8, 1);
-	color_dst = cvCreateImage(cvGetSize(src), 8, 3);
-
-	Canny(src, dst, 50, 200, 3);
-
-	cvtColor(dst, color_dst, COLOR_GRAY2BGR);
-	lines = houghLines2(dst, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI / 180, 50, 50, 10);
-
-	for (i = 0; i < lines->total; i++) {
-		Point* line = (Point*)cvGetSeqElem(lines, i);
-		Line(color_dst, line[0], line[1], CV_RGB(255, 0, 0), 3, AA, 0);
-	}
-
-	// показываем
-	namedWindow("Source", 1);
-	imshow("Source", src);
-
-	namedWindow("Hough", 1);
-	imshow("Hough", color_dst);
-
-	waitKey(0);
+	imshow("detected circles", src);
+	waitKey();
 	destroyAllWindows();
 	main();
 }
@@ -128,31 +95,19 @@ void task_2_var2()
 void task_3() 
 {
 	Mat image = chooseimg(), dst, gray, bin;
-
-	// создаём одноканальные картинки
 	gray = Mat::zeros(image.rows, image.cols, CV_8UC1);
 	bin = Mat::zeros(image.rows, image.cols, CV_8UC1);
-	// клонируем
 	dst = image;
-	// окно для отображения картинки
 	namedWindow("original", WINDOW_AUTOSIZE);
 	namedWindow("binary", WINDOW_AUTOSIZE);
 	namedWindow("contours", WINDOW_AUTOSIZE);
 
-	// преобразуем в градации серого
 	cvtColor(image, gray, COLOR_RGB2GRAY);
 
-	// преобразуем в двоичное
-	cvInRangeS(gray, Scalar(40), Scalar(150), bin); // atoi(argv[2])
+	inRange(gray, Scalar(40), Scalar(150), bin);
+	int contoursCont = findContours(bin, storage, &contours, sizeof(CvContour), RETR_LIST, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	CvSeq* contours = 0;
-
-	// находим контуры
-	int contoursCont = findContours(bin, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
-
-	// нарисуем контуры
-	for (CvSeq* seq0 = contours; seq0 != 0; seq0 = seq0->h_next) {
-		drawContours(dst, seq0, 255, CV_RGB(0, 0, 250), 0, 1, 8); // рисуем контур
+	for (CvSeq* seq0 = contours; seq0 != 0; seq0 = seq0->h_next) {/		drawContours(dst, seq0, 255, CV_RGB(0, 0, 250), 0, 1, 8);
 	}
 	imshow("original", image);
 	imshow("binary", bin);
@@ -190,9 +145,6 @@ int main()
 		task_2();
 		break;
 	case 3:
-		task_2_var2();
-		break;
-	case 4:
 		task_3();
 		break;
 	case 0:
